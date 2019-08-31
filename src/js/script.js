bb07df8976089a94803022284cd3f3aef2f58cd8
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-08-31 22:05:32 +0200

    slick slider init completed (+ jquery now properly included) & page part slider testimonials init

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,13 @@ $(document).ready( function() {
 
     console.log("JS file OK");
 
+    $(".footer-mid").css("background-color", "red");
+
+    $(".pp_slider_testimonials-item-wrapper").slick({
+     infinite: true,
+     speed: 300,
+     slidesToShow: 1,
+     adaptiveHeight: true
+     });
+
 });
