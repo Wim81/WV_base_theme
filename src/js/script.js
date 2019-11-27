805ebef18f005b5f4a535895eedf4c113b1af2bf
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-11-27 18:20:56 +0100

    large header with main navigation (with second & third level), secondary & tertiary navigation, language switch & search bar

--- src/js/script.js
+++ src/js/script.js
@@ -56,4 +56,53 @@ $(document).ready( function() {
     $('.hamburger').on("click", function() {
         $(this).toggleClass("is-active");
     });
+
+    /* header large > mobile menu > show/hide levels 2&3 */
+    $(".has-level2").on("click", function(e) {
+        $(this).toggleClass("open");
+        $(this).find(".primary-menu-level2").toggleClass("open");
+        $(".has-level2").not($(this)).removeClass("open");
+        $(".has-level2").not($(this)).find(".primary-menu-level2").removeClass("open");
+        $(".has-level2").not($(this)).find(".has-level3").removeClass("open");
+        $(".has-level2").not($(this)).find(".primary-menu-level3").removeClass("open");
+    });
+    $(".has-level3").on("click", function(e) {
+        e.stopPropagation();
+        $(this).toggleClass("open");
+        $(this).find(".primary-menu-level3").toggleClass("open");
+    });
+
+    /* desktop header large > main nav 2nd level > add class to provide space of there are expendable items */
+    $(".header-desktop-bottom .has-level3").parent().addClass("with-level3");
+
+    /* open/close search field in header */
+    $(".desktop-menu-search-logo").on("click", toggleSearchbox);
+    $(".mobile-menu-top-search-logo").on("click", toggleSearchboxMobile);
+
+    function toggleSearchbox() {
+        $(".search-form-input").toggleClass("input-visible");
+    }
+
+    function toggleSearchboxMobile() {
+        $(".search-form-input-mobile").toggleClass("input-visible");
+    }
+
+    // resize menu reset
+    $(window).on('resize', function() {
+        $(".navbar-toggler").attr("aria-expanded", false); // will reset classic Bootstrap burger menu
+        $(".navbar-toggler").removeClass("is-active"); // will reset custom animated burger menu
+        $(".primary-menu-level2").removeClass("open"); // close all 2nd level menus
+        $(".primary-menu-level3").removeClass("open"); // close all 3rd level menus
+        $(".has-level2").removeClass("open"); // reset handle which opens 2nd level menus
+        $(".has-level3").removeClass("open"); // reset handle which opens 3rd level menus
+        $(".collapse").removeClass("show"); // close 1st level menu
+    });
+
+    // large header desktop menu reset when clicking search
+    $(".desktop-menu-search-logo").on("click", function() {
+        $(".primary-menu-level2").removeClass("open"); // close all 2nd level menus
+        $(".primary-menu-level3").removeClass("open"); // close all 3rd level menus
+        $(".has-level2").removeClass("open"); // reset handle which opens 2nd level menus
+        $(".has-level3").removeClass("open"); // reset handle which opens 3rd level menus
+    });
 });
